b844c922f660f08f3eab35a6b2a449530426f9c4
mile-one-ventures <89769111+mile-one-ventures@users.noreply.github.com>
2021-09-09 18:06:09 +0100

    Update ga.js

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,10 +1,13 @@
 {% if jekyll.environment == "production" %}       
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{site.ganalytics}}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5BPEEBWFTL"></script>
 <script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '{{site.ganalytics}}');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5BPEEBWFTL');
 </script>
 {% endif %}
+
+
